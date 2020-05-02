a7484ddab8ebfc966e738a846deb16b212781cc1
JonathanChen <jonny29009@yahoo.com>
2020-05-01 23:48:25 -0700

    adding all info

---
+++ javascript/index.js
@@ -0,0 +1,9 @@
+// Sticky Header
+$(window).scroll(function () {
+
+    if ($(window).scrollTop() > 100) {
+        $('.main-header').addClass('sticky');
+    } else {
+        $('.main-header').removeClass('sticky');
+    }
+});
